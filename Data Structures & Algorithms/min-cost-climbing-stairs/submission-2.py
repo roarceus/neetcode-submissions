12762eff98e4bfcec850da5f012c6ec196011bda
Sohan <roarceus@users.noreply.github.com>
2026-04-14 01:26:17 -0400

    Add: min-cost-climbing-stairs - submission-2

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-2.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        # # Top Down (Memoization)
+        # memo = [-1] * len(cost)
+
+        # def dfs(i):
+        #     if i >= len(cost):
+        #         return 0
+        #     if memo[i] != -1:
+        #         return memo[i]
+        #     memo[i] = cost[i] + min(dfs(i + 1), dfs(i + 2))
+        #     return memo[i]
+
+        # return min(dfs(0), dfs(1))
+
+        # Bottom Up (Tabulation)
+        n = len(cost)
+        dp = [0] * (n + 1)
+
+        for i in range(2, n + 1):
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+
+        return dp[n]
